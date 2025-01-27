a9886fe2509943a66f66ad9a4b4f7409460d3da0
AxelC831 <axeldanielck@gmail.com>
2025-01-27 17:11:10 -0500

    fixed

--- react_1/src/App.js
+++ react_1/src/App.js
@@ -1,35 +1,23 @@
-import "./styles.css"
+import "./styles.css";
 import Card from "./Components/Card";
 import { useState } from "react";
 
-export default function App()   {
- const [upvote, setUpvote] = useState(0);
+export default function App() {
+  const [upvote, setUpvote] = useState(0);
 
- function like() {
-setUpvote (upvote + 1);
- }
- const name = "Axel";
-  
- return (
-   <div className="App">
-      {upvote}
+  function like() {
+    setUpvote(upvote + 1);
+  }
+
+  return (
+    <div className="App">
+      <h1>Like Counter</h1>
+      <p>{upvote}</p>
       <button onClick={like}>👍🏼</button>
-   <Card
-         Name="Axel"
-         Age= "17"
-   />
-   <Card
-         Name="Camila"
-         Age= "17" 
-   />
-   <Card
-         Name="Evangelina"
-         Age= "16"
-   />
-   <Card
-            Name="Daniel"
-            Age= "16"
-   />
-   </div>
-)
-;}
+      <Card Name="Axel" Age="17" />
+      <Card Name="Camila" Age="17" />
+      <Card Name="Evangelina" Age="16" />
+      <Card Name="Daniel" Age="16" />
+    </div>
+  );
+}
--- react_1/src/Components/Card.js
+++ react_1/src/Components/Card.js
@@ -1,20 +1,18 @@
+import "./card.css";
+
 function Heading() {
-    return <h1>My website </h1>;
+  return <h1>My Website</h1>;
 }
 
-
-
-
-function Card(props )  {
-return (
-
-    <div className="imagemy-style">
-        {}
-        <h2>{props.name}</h2>
-        <img src="TOMMY.webp" alt="avatar_img" />
-        <p> {props.tel} </p>
-        <p>{props.email}</p>
+function Card(props) {
+  return (
+    <div className="card">
+      <Heading />
+      <h2>{props.Name}</h2>
+      <img src="TOMMY.webp" alt="avatar_img" />
+      <p>Age: {props.Age}</p>
     </div>
-    );
+  );
 }
 
+export default Card;
